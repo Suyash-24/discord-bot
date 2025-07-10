826a345d1d0d0f701fef29a1ad0da6994c4fb6ed
Suyash-24 <iamnarawadesuyash@gmail.com>
2025-07-10 16:16:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,22 @@
-import os
-from dotenv import load_dotenv
 import discord
 
-  # Loads from .env file locally
 
-TOKEN = os.getenv("BOT_TOKEN")  # Grabs token from env var
+
+TOKEN = "MTM5MjQ1NTQ2NTU5MzQ3MTAxNw.GTxKTy.3-uyptW6_bEnL7UfZEQiA0iJd9NzWw_WOWFUq0"
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f"Bot is online as {client.user}")
+    print(f"Bot is ready and logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.lower() == "ping":
+        await message.channel.send("pong")
 
 client.run(TOKEN)

7d0e1dd40bffdddaef8cc7b0022c86cd33f2afb2
Suyash-24 <iamnarawadesuyash@gmail.com>
2025-07-11 23:21:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,17 +13,18 @@ else:
     prefix_db = {}
 
 NO_PREFIX_USERS = [1105502119731150858]  # Replace with actual user IDs
-
 def get_prefix(bot, message):
-    
     if not message.guild:
-        return commands.when_mentioned_or(DEFAULT_PREFIX)(bot, message)
+        return DEFAULT_PREFIX  # DMs fallback
 
-    
+    guild_id = str(message.guild.id)
+    custom_prefix = prefix_db.get(guild_id, DEFAULT_PREFIX)
+
+    # Allow both prefix and no prefix if user is in the whitelist
     if message.author.id in NO_PREFIX_USERS:
-        return commands.when_mentioned_or("", prefix_db.get(str(message.guild.id), DEFAULT_PREFIX))(bot, message)
+        return commands.when_mentioned_or("", custom_prefix)(bot, message)
 
-    return commands.when_mentioned_or(prefix_db.get(str(message.guild.id), DEFAULT_PREFIX))(bot, message)
+    return commands.when_mentioned_or(custom_prefix)(bot, message)
 
 
 intents = discord.Intents.default()
@@ -51,6 +52,12 @@ async def ping(ctx):
 async def slash_ping(interaction: discord.Interaction):
     latency = round(bot.latency * 1000)
     await interaction.response.send_message(f"Pong! Latency: `{latency}ms`")
+
+
+@bot.command()
+async def ping(ctx):
+    print("Prefix command triggered")
+    await ctx.send("pong with prefix")
 
 
 @bot.command(name="no_prefix")
